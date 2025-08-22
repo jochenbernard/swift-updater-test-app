fb8150438b88605c5e17c522b8c9be8b80b2fd0e
Jochen Bernard <jochenbernard@gmail.com>
2025-08-22 17:35:51 +0200

    Initial Commit

---
+++ SwiftUpdaterTestApp/SwiftUpdaterTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUpdaterTestAppApp.swift
+//  SwiftUpdaterTestApp
+//
+//  Created by Jochen Bernard on 22/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUpdaterTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
